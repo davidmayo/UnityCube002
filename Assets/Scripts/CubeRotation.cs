fa6e27668dd2dfcabaf5cc0396e965d824156f67
David Mayo <dcmayo@gmail.com>
2021-11-04 18:38:13 -0400

    CubeRotation.cs: Remove unneeded methods and add comments

--- Assets/Scripts/CubeRotation.cs
+++ Assets/Scripts/CubeRotation.cs
@@ -2,18 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class to represent a single rotation of a Cube
+/// </summary>
 public class CubeRotation //: MonoBehaviour
 {
+    /// <summary>
+    /// The axis to rotate around, using transform.RotateAround()
+    /// Unity has a left-handed coordinate system, so this might be unexpected
+    /// </summary>
     public Vector3 RotationAxis { get; private set; }
+
+    /// <summary>
+    /// A single character string representing which face (or face-like collection of pieces)
+    /// should be rotated.
+    /// 
+    /// Valid facelike:
+    /// U, D, F, B, L, R = face turns
+    /// u, d, f, b, l, r = wide face turns
+    /// M, E, S = slices
+    /// x, y, z = whole cube
+    /// </summary>
     public string FaceLike { get; private set; }
+
+    /// <summary>
+    /// The nonnegative number of degrees needed to make the turn. 0, 90, 180, 270, or 360.
+    /// </summary>
     public float Angle { get; private set; }
+
+    /// <summary>
+    /// The nonnegative number of quarter turns (90 degree turns) needed to make the turn. 0, 1, 2, 3, or 4.
+    /// </summary>
     public int QuarterTurns { get; private set; }
+
+    /// <summary>
+    /// Multiplier for which way to rotate
+    /// +1 for clockwise
+    /// -1 for counterclockwise
+    /// 0 for null move
+    /// </summary>
     public int Direction { get; private set; }
+
+    /// <summary>
+    /// A string representing the move in something like standard notation:
+    /// [facelike char] [OPTIONAL: ' char for inverse] [OPTIONAL: quarterturn count modifier char: 1, 2, 3, or 4]
+    /// </summary>
     public string MoveString { get; private set; }
-    public List<GameObject> Pieces { get; private set; }
 
-    private CubeLocator loc;
-
+    /// <summary>
+    /// Construct a new rotation from a standard notation string
+    /// </summary>
+    /// <param name="moveString">A move in standard notation, or 0 for a null move</param>
     public CubeRotation( string moveString)
     {
         // Null move is special
@@ -25,33 +64,27 @@ public class CubeRotation //: MonoBehaviour
             QuarterTurns = 0;
             Angle = 0f;
             Direction = 0;
-            Pieces = new List<GameObject>();
-            
             return;
         }
         MoveString = moveString;
 
-        loc = new CubeLocator();
-
+        // FaceLike will always be the first char of MoveString
         FaceLike = MoveString.Substring(0, 1);
-
-        Pieces = loc.GetPieces(FaceLike);
-
-        if( Pieces.Count == 0 )
-        {
-            throw new UnityException("Invalid rotation: " + moveString + " [No pieces selected from FaceLike " + FaceLike + ". Pass \"0\" to create a null move.]");
-        }
-
         
+        // If MoveString contains a ' char, it is inverted direction
+        // Otherwise, it's normal
         if (MoveString.Contains("'"))
             Direction = -1;
         else
             Direction = 1;
 
         // Remove the face and the apostrophe (if present), leaving only the number of turns
+        // if specified.
         // So    "U'3"   becomes    "3"
+        // If nothing specified, it's one quarter turn.
         string amountString = MoveString.Replace(FaceLike, "").Replace("'", "");
 
+        // Determine number of quarter turns.
         switch(amountString)
         {
             case "":
@@ -72,8 +105,12 @@ public class CubeRotation //: MonoBehaviour
                 //break;
         }
 
+        // Quarter turn = 90 degrees/
         Angle = 90f * QuarterTurns;
 
+        // Determing RotationAxis, which is a product of Facelike
+        // These are all empirically correct, though they don't always make physical sense
+        // because Unity has a left-handed coordinate system with right-handed rotations.
         switch(FaceLike)
         {
             case "U":
@@ -111,12 +148,18 @@ public class CubeRotation //: MonoBehaviour
                 RotationAxis = Vector3.forward;
                 break;
 
+            // Default set to <0,0,0>, which will result in no rotation
+            // But also throw an error
             default:
                 RotationAxis = Vector3.zero;
                 throw new UnityException("Unable to determine RotationAxis for FaceLike " + FaceLike);
         }
     }
 
+    /// <summary>
+    /// Get the inverse of the rotation. I.E., the rotation that would undo this rotation.
+    /// </summary>
+    /// <returns>The inverted rotation</returns>
     public CubeRotation GetInverse()
     {
         // Handle null move specially
@@ -133,13 +176,22 @@ public class CubeRotation //: MonoBehaviour
             return new CubeRotation(MoveString + "'");
     }
 
+    /// <summary>
+    /// Get the inverse of the rotation. I.E., the rotation that would undo the given rotation.
+    /// </summary>
+    /// <param name="rotation">Rotation to invert</param>
+    /// <returns>The inverted rotation</returns>
     public static CubeRotation GetInverse( CubeRotation rotation )
     {
         return rotation.GetInverse();
     }
 
+    /// <summary>
+    /// Return all the Properties in a single string
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
-        return $"CubeRotation: {MoveString}   [Direction={Direction}, RotationAxis={RotationAxis}, Angle={Angle}, QuarterTurns={QuarterTurns}, FaceLike={FaceLike}, Pieces.Count={Pieces.Count}]";
+        return $"CubeRotation: {MoveString} [Direction={Direction}, RotationAxis={RotationAxis}, Angle={Angle}, QuarterTurns={QuarterTurns}, FaceLike={FaceLike}]";
     }
 }
